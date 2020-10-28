c4d6da39eeb3d71ea2160b9fecbca8b9772f1dd3
krishnaguptaa <krishna2018gupta@gmail.com>
2020-10-27 20:27:01 -0400

    create a another div of row class and make paragraph inside this div

--- recourses/template/front end/footer.php
+++ recourses/template/front end/footer.php
@@ -1,5 +1,22 @@
 <div class="footer">
     <div class="col">
         <h1>Help</h1>
+        <div class="row">
+                <a href="#">Delivery</a>
+                <a href="#">Secure Payment</a>
+                <a href="#">Search Near By Stores</a>
+                <a href="#">Return</a>
         </div>
     </div>
+    <div class="col">
+        <a id = "linking"><h1>ABOUT US</h1></a>
+        <div class="row">
+            <p>
+                Supporting small business
+                <br>Out For Delivery<br>
+                Working at Home town grocers
+                <a href="aboutus.php">Read More</a>
+            </p>
+        </div>
+    </div>
+</div>
