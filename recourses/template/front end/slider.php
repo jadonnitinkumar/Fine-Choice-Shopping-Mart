48d6e3e6fd1a87668beded3e338eda16cc5fccc2
krishnaguptaa <krishna2018gupta@gmail.com>
2020-10-27 21:17:35 -0400

    put a image with the help of img tag

--- recourses/template/front end/slider.php
+++ recourses/template/front end/slider.php
@@ -1,7 +1,8 @@
 <div class="slide-show-container">
     <div class="myslides fade">
         <div class="numbertext">1/3</div>
-        
+        <img src="slide1.jpg">
+    </div>
     
     
     
