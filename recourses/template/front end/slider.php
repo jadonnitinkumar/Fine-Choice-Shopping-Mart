7c764ce4a7fce0c322957688c84e1c23e1049ed5
krishnaguptaa <krishna2018gupta@gmail.com>
2020-10-27 21:18:36 -0400

    put another slider images

--- recourses/template/front end/slider.php
+++ recourses/template/front end/slider.php
@@ -3,6 +3,14 @@
         <div class="numbertext">1/3</div>
         <img src="slide1.jpg">
     </div>
+    <div class="myslides fade">
+        <div class="numbertext">2/3</div>
+        <img src="slide2.jpg">
+    </div>
+    <div class="myslides fade">
+        <div class="numbertext">3/3</div>
+        <img src="slide3.jpg">
+    </div>
     
     
     
