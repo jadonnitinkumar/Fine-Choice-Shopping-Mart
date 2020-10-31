0bb685e6282d2880f1c8bdb10c3bdb060f4da205
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 19:43:58 +0530

    create login.css

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fine Choice Shopping Mart</title>
     <link rel="stylesheet" type="text/css" href="css/index.css" />
+    
 </head>
 
 <body>
