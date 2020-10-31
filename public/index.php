5bfce94b89cd63013762d9aa89cb0f56abac78d6
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 22:31:51 +0530

    create file signup.php

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@
     <title>Fine Choice Shopping Mart</title>
     <link rel="stylesheet" type="text/css" href="css/index.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
--- resources/template/front_end/header.php
+++ resources/template/front_end/header.php
@@ -7,79 +7,8 @@
             <li><b><a href="">CATEGORY</a></b></li>
             <li><b><a href="checkout.php">CART</a></b></li>
             <li><b><a href="contact.php">CONTACTS</a></b></li>
-            <li><b><a href="">LOGIN/SIGNUP</a></b>
-                <ul>
-                    <li>
-                        <button class="button" onclick="document.getElementById('id01').style.display='block'"
-                            style="width:200px;">CUSTOMER</button>
-                        <div id="id01" class="modal">
-                            <form class="modal-content animate" method="post">
-                                <span onclick="document.getElementById('id01').style.display='none'" class="close"
-                                    title="Close Modal">&times;</span>
-                                <h1
-                                    style="color: white; text-align: center; padding-top: 5px; font-size: 35px; font-family: sans-serif;">
-                                    SignUp Here</h1>
-                                <form action="" method="post">
-                                    
-                                    <div class="container">
-                                        <div class="con"><i class="fa fa-user icon" style="color:;"></i></div>
-                                        <input type="text " placeholder="Enter Your Name" name="usrnm">
-                                    </div>
-                                    <div class="container">
-                                        <div class="con"><i class="fa fa-envelope icon"></i></div>
-                                        <input type="email " placeholder="Your email Id" name="email">
-                                    </div>
-                                    <div class="container">
-                                        <div class="con"><i class="fa fa-user icon"></i></div>
-                                        <input type="text " placeholder="Enter User Name" name="uname">
-                                    </div>
-                                    <div class="container">
-                                        <div class="con"><i class="fa fa-phone icon"></i></div>
-                                        <input type="Phone " placeholder="Enter Phone Number" name="phn">
-                                    </div>
-                                    <div class="container">
-                                        <div class="con"><i class="fa fa-key icon"></i></div>
-                                        <input type="password " placeholder="Enter Password" name="psw">
-                                    </div>
-                                    <div>
-                                        <button class="btn" name="submit" type="submit ">SignUp</button>
-                                    </div>
-                                    <p style="font-size: 20px; font-family: sans-serif;">if already
-                                        account then<a href=""
-                                            style="color: white; font-size: 25px; font-family: sans-serif;">Login</a>
-                                    </p>
-                                </form>
-                            </form>
-                        </div>
-
-                        <script>
-                            
-                            var modal = document.getElementById('id01');
-                            
-                            window.onclick = function (event) {
-                                if (event.target == modal) {
-                                    modal.style.display = "none";
-                                }
-                            }
-                        </script>
-                        <button class="button" style="width:180px;"><a href="newsignup.php"
-                                style="color: aliceblue;margin-left: -20px;">SHOPKEEPER</a></button>
-
-                        <script>
-                            
-                            var modal = document.getElementById('id02');
-
-                            window.onclick = function (event) {
-                                if (event.target == modal) {
-                                    modal.style.display = "none";
-                                }
-                            }
-                        </script>
-
-                    </li>
-                </ul>
-            </li>
-
+            <li><b><a href="login.php">LOGIN</a></b></li>
+            <li><b><a href="login.php">SIGN UP</a></b></li>
         </div>
     </ul>
 </div>
