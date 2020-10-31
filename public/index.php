661ec5095fd88f28c489e288097b5b01744df626
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 18:30:24 +0530

    did some changes in class names

--- public/index.php
+++ public/index.php
@@ -11,26 +11,26 @@
 <body>
     <?php include "../resources/template/front_end/header.php"; ?>
     <?php include "../resources/template/front_end/slider.php"; ?>
-    <h1 id=line1>SHOP FOR HEALTHY FOODS</h1>
-    <div class="asdf2">
+    <h1 id=line-1>SHOP FOR HEALTHY FOODS</h1>
+    <div class="products">
         <section id="product-1">
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\Honey.jpg" alt="Honey" title="Honey">
+                    <img class="product-img" src="Screenshots\Honey.jpg" alt="Honey" title="Honey">
                 </a>
                 <h1 class="product-name">Honey</h1>
             </div>
 
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\olive.jpg" alt="Olive Oil" title="Olive Oil">
+                    <img class="product-img" src="Screenshots\olive.jpg" alt="Olive Oil" title="Olive Oil">
                 </a>
                 <h1 class="product-name">Olive-Oil</h1>
             </div>
 
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\dry-fruits-nuts.jpeg" alt="Dry-fruits-nuts"
+                    <img class="product-img" src="Screenshots\dry-fruits-nuts.jpeg" alt="Dry-fruits-nuts"
                         title="Dry-fruits-nuts">
                 </a>
                 <h1 class="product-name">Dry-Fruits</h1>
@@ -38,22 +38,22 @@
         </section>
         <section id=product-2>
 
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\cold drinks.jpg" alt="Colddrinks" title="Cold drink">
+                    <img class="product-img" src="Screenshots\cold drinks.jpg" alt="Colddrinks" title="Cold drink">
                 </a>
                 <h1 class="product-name">Green-Tea</h1>
             </div>
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\row-of-spoons-with-different-dried-pulses.jpg"
+                    <img class="product-img" src="Screenshots\row-of-spoons-with-different-dried-pulses.jpg"
                         alt="Dried-pulses" title="Dried-pulses">
                 </a>
                 <h1 class="product-name">Pulses</h1>
             </div>
-            <div class="gallery2">
+            <div class="gallery">
                 <a target="_blank" href="#">
-                    <img class="product-imag" src="Screenshots\Beans.jpg" alt="Cinque Terre" title="Beans">
+                    <img class="product-img" src="Screenshots\Beans.jpg" alt="Cinque Terre" title="Beans">
                 </a>
                 <h1 class="product-name">Beans</h1>
             </div>
