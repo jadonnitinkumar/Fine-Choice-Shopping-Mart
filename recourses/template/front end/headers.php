f2f50035efefb77a241ab487831a725b5ca1c557
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 15:33:54 +0530

    create link CAREGORY

--- recourses/template/front end/headers.php
+++ recourses/template/front end/headers.php
@@ -4,6 +4,7 @@
     <li><b><a href="index.php">HOME</a></b></li>
     <li><b><a href="dashboard/index.php">ADMIN</a></b></li>
     <li><b><a href="#linking">ABOUT-US</a></b></li>
+    <li><b><a href="">CATEGORY</a></b></li>
     </div>
     </ul>
 </div>
