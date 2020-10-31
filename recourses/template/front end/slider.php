4ae79225262f7f42677c6c9af10cc1dd4f486e21
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 17:30:06 +0530

    Did spme path changes

--- recourses/template/front end/slider.php
+++ recourses/template/front end/slider.php
@@ -1,20 +1,17 @@
-<div class="slide-show-container">
-    <div class="myslides fade">
-        <div class="numbertext">1/3</div>
-        <img src="../recourses/template/front end/slider image/Slide1.jpg">
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <img src="Slides\Slide1.jpg">
     </div>
-    <div class="myslides fade">
-        <div class="numbertext">2/3</div>
-        <img src="../recourses/template/front end/slider image/Slide2.jpg">
+    <div class="mySlides fade">
+        <img src="Slides\Slide1.jpg">
     </div>
-    <div class="myslides fade">
-        <div class="numbertext">3/3</div>
-        <img src="../recourses/template/front end/slider image/Slide3.jpg">
+    <div class="mySlides fade">
+        <img src="Slides\Slide1.jpg">
     </div>
 </div>
 <br><br>
 <div id="dots">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
 </div>
