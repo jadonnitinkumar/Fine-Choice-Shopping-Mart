4641f7f117ea6a0a7c04c81caf3d7b83827f0edf
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 16:28:18 +0530

    add footer.php

--- public/index.php
+++ public/index.php
@@ -1,59 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fine Choice Shopping Mart</title>
 </head>
+
 <body>
-<?php include "../recourses/template/front end/headers.php"; ?>
-<?php include "../recourses/template/front end/slider.php"; ?>
+    <?php include "../recourses/template/front end/headers.php"; ?>
+    <?php include "../recourses/template/front end/slider.php"; ?>
     <h1 id=line1>SHOP FOR HEALTHY FOODS</h1>
     <div class="asdf2">
-    <section id="product-1">
-    <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\Honey.jpg" alt="Honey" title="Honey">
-            </a>
-            <h1 class="product-name">Honey</h1>
-        </div>
+        <section id="product-1">
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\Honey.jpg" alt="Honey" title="Honey">
+                </a>
+                <h1 class="product-name">Honey</h1>
+            </div>
 
-        <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\olive.jpg" alt="Olive Oil" title="Olive Oil">
-            </a>
-            <h1 class="product-name">Olive-Oil</h1>
-        </div>
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\olive.jpg" alt="Olive Oil" title="Olive Oil">
+                </a>
+                <h1 class="product-name">Olive-Oil</h1>
+            </div>
 
-        <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\dry-fruits-nuts.jpeg" alt="Dry-fruits-nuts"
-                    title="Dry-fruits-nuts">
-            </a>
-            <h1 class="product-name">Dry-Fruits</h1>
-        </div>
-    </section>
-    <section id=product-2>
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\dry-fruits-nuts.jpeg" alt="Dry-fruits-nuts"
+                        title="Dry-fruits-nuts">
+                </a>
+                <h1 class="product-name">Dry-Fruits</h1>
+            </div>
+        </section>
+        <section id=product-2>
 
-        <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\cold drinks.jpg" alt="Colddrinks" title="Cold drink">
-            </a>
-            <h1 class="product-name">Green-Tea</h1>
-        </div>
-        <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\row-of-spoons-with-different-dried-pulses.jpg" alt="Dried-pulses" title="Dried-pulses">
-            </a>
-            <h1 class="product-name">Pulses</h1>
-        </div>
-        <div class="gallery2">
-            <a target="_blank" href="#">
-                <img class="product-imag" src="Screenshots\Beans.jpg" alt="Cinque Terre" title="Beans">
-            </a>
-            <h1 class="product-name">Beans</h1>
-        </div>
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\cold drinks.jpg" alt="Colddrinks" title="Cold drink">
+                </a>
+                <h1 class="product-name">Green-Tea</h1>
+            </div>
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\row-of-spoons-with-different-dried-pulses.jpg"
+                        alt="Dried-pulses" title="Dried-pulses">
+                </a>
+                <h1 class="product-name">Pulses</h1>
+            </div>
+            <div class="gallery2">
+                <a target="_blank" href="#">
+                    <img class="product-imag" src="Screenshots\Beans.jpg" alt="Cinque Terre" title="Beans">
+                </a>
+                <h1 class="product-name">Beans</h1>
+            </div>
         </section>
     </div>
+    <div class="healthy">
+        <img class="healthy-img" src="Screenshots\Blog_thumb.jpg">
+        <h1>To Get Tight, Eat Right!</h1>
+    </div>
+
+    <?php include "../recourses/template/front end/footer.php"; ?>
 </body>
+
 </html>
