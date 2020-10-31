1e300d113200c639dd878f443805d442dafc1644
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 16:16:54 +0530

    complete product-1

--- public/index.php
+++ public/index.php
@@ -8,7 +8,29 @@
 <body>
     <h1 id=line1>SHOP FOR HEALTHY FOODS</h1>
     <div class="asdf2">
-    <section id="product-1"></section>
+    <section id="product-1">
+    <div class="gallery2">
+            <a target="_blank" href="#">
+                <img class="product-imag" src="Screenshots\Honey.jpg" alt="Honey" title="Honey">
+            </a>
+            <h1 class="product-name">Honey</h1>
+        </div>
+
+        <div class="gallery2">
+            <a target="_blank" href="#">
+                <img class="product-imag" src="Screenshots\olive.jpg" alt="Olive Oil" title="Olive Oil">
+            </a>
+            <h1 class="product-name">Olive-Oil</h1>
+        </div>
+
+        <div class="gallery2">
+            <a target="_blank" href="#">
+                <img class="product-imag" src="Screenshots\dry-fruits-nuts.jpeg" alt="Dry-fruits-nuts"
+                    title="Dry-fruits-nuts">
+            </a>
+            <h1 class="product-name">Dry-Fruits</h1>
+        </div>
+    </section>
     </div>
 </body>
 </html>
