0051a6279d3f75032f9982c3121742cc31787ec9
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-10-31 15:32:36 +0530

    create link ABOUT-US

--- recourses/template/front end/headers.php
+++ recourses/template/front end/headers.php
@@ -3,6 +3,7 @@
     <div class="iii">
     <li><b><a href="index.php">HOME</a></b></li>
     <li><b><a href="dashboard/index.php">ADMIN</a></b></li>
+    <li><b><a href="#linking">ABOUT-US</a></b></li>
     </div>
     </ul>
 </div>
