367522f88b37a40e4879b35fc08c624a90a9f572
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-11-22 00:14:50 +0530

    create categories.php

---
+++ resources/template/front_end/categories.php
@@ -0,0 +1,51 @@
+<div class="asdf1">
+    <a id="linking1"><h1 style="color: red; text-align: center;">FEATURED CATEGORIES</h1></a>
+    <div class="gallery1" style="height: auto;margin-left: 100px;">
+        <a href="bread.html">
+            <img class="img-circle" src="home-img-btm.png" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1"></div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="dairy.html">
+            <img class="img-circle" src="Round-up-of-October-s-new-dairy-products_wrbm_large.jpg" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Dairy Products</div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="beverages.html">
+            <img class="img-circle" src="beverages.jpg" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Beverages</div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="cleaning.html">
+            <img class="img-circle" src="images.jpg" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Clean Supplies</div>
+    </div>
+    <div class="gallery1" style="height: auto;margin-left: 100px;">
+        <a href="snacks.html">
+            <img class="img-circle" src="chat_mobile-banner_COMPRESSED.png" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Snack Foods</div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="rice.html">
+            <img class="img-circle" src="1567517227575_0.webp" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Rice,flour & Pulses</div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="chocolate.html">
+            <img class="img-circle" src="670x770.jpg" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Sweets and Chocolates</div>
+    </div>
+    <div class="gallery1" style="height: auto;">
+        <a href="noodles.html">
+            <img class="img-circle" src="noodles.jpeg" alt="Cinque Terre" style="width: 200px;">
+        </a>
+        <div class="desc1">Pasta & Noodles</div>
+    </div>
+</div>
