55c95a040ba3777e3f0d7278530c7e8125d496ae
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-11-22 00:09:22 +0530

    add unordered list

--- resources/template/front_end/header.php
+++ resources/template/front_end/header.php
@@ -1,15 +1,23 @@
 <?php require_once("../resources/config.php"); ?>
 <div class="navbar">
     <ul>
-        <div class="bar">
+        <div class="iii">
             <li><b><a href="index.php">HOME</a></b></li>
             <li><b><a href="dashboard/index.php">ADMIN</a></b></li>
             <li><b><a href="#linking">ABOUT-US</a></b></li>
-            <li><b><a href="">CATEGORY</a></b></li>
-            <li><b><a href="checkout.php">CART</a></b></li>
+            <div class="dropdown">
+                <button class="dropbtn"><b><a href="#linking1">CATEGORY</a></b></button>
+                <div class="dropdown-content">
+                    <?php get_categories_in_dropdown(); ?>
+                </div>
+            </div>
+            <li><b><a href="checkout.php">CART</a></b>
             <li><b><a href="contact.php">CONTACTS</a></b></li>
-            <li><b><a href="login.php">LOGIN</a></b></li>
-            <li><b><a href="Signup.php">SIGNUP</a></b></li>
+            <?php if(isset($_SESSION['email'])){?>
+            <li><b><a href="">Login | Register</a></b>
+                <?php
+				}else{
+				echo '<li><b><a href=""><?php echo $_SESSION["name"]; ?> Login | Register</a></b>';
+                }?>
+                <ul>
         </div>
-    </ul>
-</div>
