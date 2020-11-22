00075a08bd8c39efbe673c4c272f2a0e9c275da1
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:45:17 -0500

    create carts function

--- resources/cart.php
+++ resources/cart.php
@@ -48,3 +48,69 @@ if(isset($_GET['delete'])){
     redirect("../public/checkout.php");
     
     }
+    function carts(){
+        $total = 0;
+        $item_quantity = 0;
+        $item_name = 1;
+        $item_number = 1;
+        $amount = 1;
+        $quantity = 1;
+        
+        foreach ($_SESSION as $name => $value) {
+        
+        if($value > 0){
+        
+        if(substr($name, 0, 8 ) == "product_"){
+        
+        $length = strlen($name);
+        
+        $id = substr($name, 8 , $length);
+        
+        $query = query("SELECT * FROM products WHERE product_id = " . escape_string($id) . " ");
+        confirm($query);
+        
+        while($row = fetch_array($query)){
+        
+            $sub = $row['product_price']*$value;
+            $item_quantity +=$value;
+        
+            $product_image = display_image($row['product_image']);
+        
+        $product = <<<DELIMETER
+                     <tr>
+                        <td>{$row['product_title']}
+                        </td>
+                        <td>Rs {$row['product_price']}</td>
+                        <td>{$value}</td>
+                        <td>Rs {$sub}</td>
+                        <td><a class= 'btn btn-warning' href="../resources/cart.php?remove={$row['product_id']}"><span class = 'glyphion glyphion-minus'>-</span></a>
+                        <a class ='btn btn-success'href="../resources/cart.php?add={$row['product_id']}"><span class = 'glyphion glyphion-plus'>+</span></a>
+                        <a class ='btn btn-danger' href="../resources/cart.php?delete={$row['product_id']}"><span class = 'glyphion glyphion-remove'>x</span></a></td>
+                        <td></a></td>
+                      
+                    </tr>
+        
+          <input type="hidden" name="item_name_{$item_name}" value="{$row['product_title']}">
+          <input type="hidden" name="item_number_{$item_number}" value="{$row['product_id']}">
+          <input type="hidden" name="amount_{$amount}" value="{$row['product_price']}">
+          <input type="hidden" name="quantity_{$quantity}" value="{$value}">
+        
+        DELIMETER;
+        
+        
+        echo $product;
+        
+        $item_name++;
+        $item_number++;
+        $amount++;
+        $quantity++;
+        
+        }
+        
+        $_SESSION['item_total'] = $total += $sub;
+        $_SESSION['item_quantity'] = $item_quantity;
+        
+}
+}
+}
+}
