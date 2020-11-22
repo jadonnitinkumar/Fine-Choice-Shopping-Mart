1284adf62cfab8126b85dfd4e6223b46d566b2a1
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-11-22 18:05:05 +0530

    delete order

---
+++ resources/template/back_end/delete_category.php
@@ -0,0 +1,16 @@
+<?php require_once("../../config.php");
+
+if(isset($_GET['id'])){
+	
+$query = query("DELETE FROM categories WHERE cat_id = " . escape_string($_GET['id'])."");
+confirm($query);
+
+set_message("Category Deleted");
+redirect("../../../public/dashboard/index.php?categories");
+
+
+}else{
+
+redirect("../../../public/dashboard/index.php?categories");
+}
+?>
