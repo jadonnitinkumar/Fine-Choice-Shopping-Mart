923de086dddc5aa14c59b0ccc95cce68fa3b4963
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:42:25 -0500

    add some php regarding products

--- resources/cart.php
+++ resources/cart.php
@@ -1,1 +1,25 @@
 <?php require_once("config.php"); ?>
+<?php
+    if(isset($_GET['add'])){
+
+	$query =query("SELECT * FROM products WHERE product_id =" . escape_string($_GET['add']) . " ");
+	confirm($query);
+
+	while($row = fetch_array($query)){
+
+		if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]){
+
+			$_SESSION['product_' . $_GET['add']] +=1;
+			redirect("../public/checkout.php");
+
+		}else{
+
+			set_message("We only have " . $row['product_quantity'] . " {$row['product_title']}" . " available");
+			redirect("../public/checkout.php");
+		}
+    }
+
+// $_SESSION['product_' . $_GET['add']] +=1;
+
+// redirect("index.php");
+}
