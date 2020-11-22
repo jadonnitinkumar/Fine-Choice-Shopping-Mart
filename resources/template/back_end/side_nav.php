aacea32da6a422c36ca95538d3fc1a9db02b6206
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-11-22 21:45:07 +0530

    add side_nav.php

---
+++ resources/template/back_end/side_nav.php
@@ -0,0 +1,27 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav" style="background-color: rgb(204,110,110);">
+
+                    <li class="">
+                        <a href="index.php?orders" style="color: white" style="background-color: rgb(204,110,110);"><i class="fa fa-fw fa-dashboard"></i> Orders</a>
+                    </li>
+                    <li>
+                        <a href="index.php?reports" style="color: white"><i class="fa fa-fw fa-bar-chart-o"></i> Reports</a>
+                    </li>
+                    <li>
+                        <a href="index.php?products" style="color: white"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
+                    </li>
+                    <li>
+                        <a href="index.php?add_product" style="color: white"><i class="fa fa-fw fa-table"></i> Add Product</a>
+                    </li>
+                    
+                    <li>
+                        <a href="index.php?categories" style="color: white"><i class="fa fa-fw fa-desktop"></i> Categories</a>
+                    </li>
+                    <li>
+                        <a href="index.php?users" style="color: white"><i class="fa fa-fw fa-wrench"></i>Users
+                        </a>
+                    </li>
+                
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
