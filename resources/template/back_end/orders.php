2393efd7f9c0625cc199b8fcfb9d770f3e01c039
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-11-22 21:42:47 +0530

    add orders.php

---
+++ resources/template/back_end/orders.php
@@ -0,0 +1,27 @@
+
+<div class="col-md-12">
+<div class="row">
+<h1 class="page-header">
+   ALL ORDERS
+</h1>
+<h3 class ="bg-success"><?php display_message(); ?></h3>
+</div>
+<div class="row">
+<table class="table table-hover">
+    <thead>
+          <tr>
+           <th>Id</th>
+           <th>Amount</th>
+           <th>Transaction</th>
+           <th>Currency</th>
+           <th>Status</th>
+           <th>Date</th>
+           <th>Time</th>
+          </tr>
+    </thead>
+    <tbody>
+        <?php display_orders(); ?>
+    </tbody>
+</table>
+</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>
