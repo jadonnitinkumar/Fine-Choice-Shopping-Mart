d4a8f3b8a5a161100b39e07f39c2fe3f3ddb1054
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:43:19 -0500

    did some changes

--- resources/cart.php
+++ resources/cart.php
@@ -23,3 +23,28 @@
 
 // redirect("index.php");
 }
+if(isset($_GET['remove'])){
+
+	$_SESSION['product_' . $_GET['remove']]--;
+
+	if($_SESSION['product_' . $_GET['remove']] < 1){
+
+		unset($_SESSION['item_total']);
+		unset($_SESSION['item_quantity']);
+
+		redirect("../public/checkout.php");
+	}else
+	{
+
+		redirect("../public/checkout.php");
+	}
+}
+if(isset($_GET['delete'])){
+    $_SESSION['product_' . $_GET['delete']] = '0';
+    unset($_SESSION['item_total']);
+    unset($_SESSION['item_quantity']);
+    
+    
+    redirect("../public/checkout.php");
+    
+    }
