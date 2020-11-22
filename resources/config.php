d69a2b3caf31dcbaeee6e240e1f6855f99242940
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:50:13 -0500

    write try catch bock

--- resources/config.php
+++ resources/config.php
@@ -12,3 +12,14 @@ defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 require_once("function.php");
 require_once("cart.php");
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+
+
+?>
