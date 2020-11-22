0f6556670d54e082b3b0c541aece0b3b6d92939f
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:54:30 -0500

    create function for products

--- resources/function.php
+++ resources/function.php
@@ -49,3 +49,45 @@ function last_id(){
     }
     
     }
+    function escape_string($string){
+
+        global $connection;
+    
+        return mysqli_real_escape_string($connection,$string);
+    }
+    
+    function fetch_array($result){
+    
+        return mysqli_fetch_array($result);
+    }
+
+//get products
+
+function get_products(){
+
+	$query = query(" SELECT * FROM products where product_category_id<=104");
+	confirm($query);
+
+while($row = fetch_array($query)){
+
+  $product_image = display_image($row['product_image']);
+
+	$product = <<<DELIMETER
+
+
+    <div class="gallery">
+        <a target="_blank" href="item.php?id={$row['product_id']}">
+            <img src="../resources/{$product_image}" alt="Cinque Terre">
+        </a>
+        <button class="button">
+            <a href="../resources/cart.php?add={$row['product_id']}"><i class="fa fa-shopping-cart" style="color:red;font-size: 20px;"></i>ADD</a>
+        </button>
+        <div class="desc">{$row['product_title']}</div>
+    </div> 
+
+
+DELIMETER;
+	echo $product;
+}
+
+}
