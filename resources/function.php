93583738aa11f17bca4ce4e8830eaa012db7586c
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:51:58 -0500

    create set message function for message

--- resources/function.php
+++ resources/function.php
@@ -8,3 +8,13 @@ function redirect($location){
 
 	return header("Location: $location ");
 }
+function set_message($msg){
+
+    if(!empty($msg)){
+
+        $_SESSION['message'] = $msg;
+    }else{
+
+        $msg = "";
+    }
+}
