08f163fd58d605e0f9cb082655ab0abc594f2f3e
krishnaguptaa <krishna2018gupta@gmail.com>
2020-11-22 14:53:21 -0500

    create some another functions

--- resources/function.php
+++ resources/function.php
@@ -26,3 +26,26 @@ function display_message(){
         unset($_SESSION['message']);
     }
 }
+function last_id(){
+    global $connection;
+    
+    return mysqli_insert_id($connection);
+    }
+    
+    function query($sql){
+    
+        global $connection;
+    
+        return mysqli_query($connection, $sql);
+    }
+    
+    
+    function confirm($result){
+    
+    global $connection;
+    if(!$result){
+        
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+    
+    }
